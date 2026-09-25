634251834727382ff9890d9bbbaa265e7f2c695f
Juliana Aparecida Vieira de Souza <julianaap02@gmail.com>
2026-09-24 23:38:20 -0300

    chore: teste para revert

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ const hora = new Date().getHours();
 const saudacao = hora < 12 ? "Bom dia" : hora < 18 ? "Boa tarde" : "Boa noite";
 const h1 = document.querySelector("h1");
 h1.textContent = `${saudacao}, Professor! 👋`;
+// teste temporario
