8587582098350ec0e96a46b7f00226fdb2c8ae67
Juliana Aparecida Vieira de Souza <julianaap02@gmail.com>
2026-09-24 23:10:14 -0300

    style: altera cor do badge

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Saudação dinâmica baseada no horário
+
 const hora = new Date().getHours();
 const saudacao = hora < 12 ? "Bom dia" : hora < 18 ? "Boa tarde" : "Boa noite";
 const h1 = document.querySelector("h1");
